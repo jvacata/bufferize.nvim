a73b89487c1bd56df20aab8e52196783c1e4965c
Jan Vacata <jan@vacata.cz>
2026-01-23 13:15:50 +0100

    Test test fail

--- tests/execute_command_spec.lua
+++ tests/execute_command_spec.lua
@@ -2,6 +2,6 @@ local bufferize = require('bufferize.init')
 
 describe('Execute command', function()
     it('Should execute vim command', function()
-        assert.are.same("test result", bufferize.execute_command('echo "test result"'))
+        assert.are.same("test results", bufferize.execute_command('echo "test result"'))
     end)
 end)
